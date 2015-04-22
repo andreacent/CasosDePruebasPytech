0aa29f51231a40ba2fead64e1c5f89d90be373b3
andrea <andreacent8@gmail.com>
2015-04-22 10:26:19 -0430

    commit

---
+++ tarea2/pruebas.py
@@ -0,0 +1,15 @@
+'''
+Suite de casos de pruebas para la funcion CalcularPrecio() 
+Creado el 22/04/2015
+Equipo:Pytech
+Autores:
+Andrea Centeno 10-10138
+Jose Patiño 11-10745
+Andrea Cutillas 11-11145
+'''
+
+import unittest
+from tarea2 import * 
+        
+if __name__ == "__main__":
+    unittest.main()
