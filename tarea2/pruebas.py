1e47c322fd407ddff103d381e02ede535b621c3b
andrea <andreacent8@gmail.com>
2015-04-22 21:33:02 -0430

    pruebas

--- tarea2/pruebas.py
+++ tarea2/pruebas.py
@@ -16,9 +16,9 @@ class TestCalcularPrecio(unittest.TestCase):
     
     '''FRONTERAS'''
     
-    '''Revisa el tiempo de reserva de 14:59 minutos'''
-    def testDomTarifa(self):
-        tiempo = [datetime(2015,1,1,8,0,0),datetime(2015,1,1,8,14,59)]
+    #Tiempo de reserva: 14 minutos con 59 segundos
+    def testCalcularPecioA(self):
+        tiempo = [datetime(2015,1,1,8),datetime(2015,1,1,8,14,59)]
         tarifa = Tarifa(0,0)
         try:
             calcularPrecio(tarifa,tiempo)
@@ -26,8 +26,33 @@ class TestCalcularPrecio(unittest.TestCase):
         else: 
             self.fail("Resultado inesperado")
             
-    '''Revisa el tiempo de reserva de 15 minutos'''
-  
+    #Tiempo de reserva de 15 minutos
+    def testCalcularPecioB(self):
+        tiempo = [datetime(2015,1,1,8),datetime(2015,1,1,8,15)]
+        tarifa = Tarifa(1,1)
+        try:
+            calcularPrecio(tarifa,tiempo)
+        except: 
+            self.fail("Resultado inesperado")
+            
+    #Tiempo de reserva: 7 dias y un segundo
+    def testCalcularPecioA(self):
+        tiempo = [datetime(2015,1,10),datetime(2015,1,17,0,0,1)]
+        tarifa = Tarifa(0,0)
+        try:
+            calcularPrecio(tarifa,tiempo)
+        except: pass
+        else: 
+            self.fail("Resultado inesperado")
+            
+    #Tiempo de reserva: 7 dias
+    def testCalcularPecioB(self):
+        tiempo = [datetime(2015,1,10),datetime(2015,1,17)]
+        tarifa = Tarifa(1,1)
+        try:
+            calcularPrecio(tarifa,tiempo)
+        except: 
+            self.fail("Resultado inesperado")
             
 if __name__ == "__main__":
     unittest.main()
