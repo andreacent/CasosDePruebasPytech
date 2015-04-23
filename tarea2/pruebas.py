bd3401291a1791dde8dbd818f32f75ac65cad8ca
Daniel <dtb_94@hotmail.com>
2015-04-23 00:30:30 -0430

    4 casos

--- tarea2/pruebas.py
+++ tarea2/pruebas.py
@@ -22,11 +22,19 @@ class TestCalcularPrecio(unittest.TestCase):
         fechas1=[datetime(2015,4,25),datetime(2015,4,25,0,16)]
         try:
             calcularPrecio(Tarifa(-0.00001,0.0002),fechas1)
-        except: pass
+        except: pass #Se levanta la excepcion (resultado esperado)
         else: 
             self.fail("Resultado inesperado")
             
-           
+    def testFueraRangoB (self): 
+        #Analiza el caso donde las tarifas son negativas
+        fechas1=[datetime(2015,4,25),datetime(2015,4,25,0,16)]
+        try:
+            calcularPrecio(Tarifa("a","b"),fechas1)
+        except: pass #Se levanta la excepcion (resultado esperado)
+        else: 
+            self.fail("Resultado inesperado")
+                   
     def testMaliciaA(self):
         #Probamos calcular la tarifa cruzando con un ano bisiesto
         fechaBisiesta=[datetime(2016,2,28),datetime(2016,3,1)]
